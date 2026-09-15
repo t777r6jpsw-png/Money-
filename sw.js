4a82bbcd6620429d82b46f12d3ee3c2cba51a5f8
t777r6jpsw-png <t777r6jpsw@privaterelay.appleid.com>
2026-09-16 00:00:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='moneymate-v8-email';
+const CACHE='moneymate-v8-email-2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
